9f448fe878e35a104c529155655fef0456cd3530
aacebo <aacebowork@gmail.com>
2024-01-10 16:59:59 -0500

    Revert "Update send.ts"
    This reverts commit f3a0cd9a68fd61ee7f244b5b5da8129cd43f6ef6.

--- src/actions/send.ts
+++ src/actions/send.ts
@@ -4,27 +4,28 @@ import { Form } from '../models';
 
 export function send(app: App) {
   return async ({ session_id, chat, user, value, ack }: ActionHandlerArgs<'form', Form>['chat']) => {
-    try {
-      await app.api.views.dialogs.close(session_id, 'polls.create');
-      await app.api.messages.createFor(user.name, chat.id, {
-        visibility: 'public',
+    await app.api.views.dialogs.close(session_id, 'polls.create');
+    await app.api.messages.createFor(user.name, chat.id, {
+      visibility: 'public',
+      child: {
+        type: 'container',
+        padding: {
+          top: 5,
+          right: 5,
+          bottom: 5,
+          left: 5
+        },
         child: {
-          type: 'container',
-          padding: {
-            top: 5,
-            right: 5,
-            bottom: 5,
-            left: 5
-          },
-          child: {
-            type: 'column',
-            children: [
-              {
-                type: 'title',
-                text: value.title
-              },
-              ...Object.values(value.options).map((o, i) => ({
-                id: i.toString(),
+          type: 'column',
+          children: [
+            {
+              type: 'title',
+              text: value.title
+            },
+            ...Object.values(value.options).map((o, i) => ({
+              id: i.toString(),
+              type: 'button',
+              child: {
                 type: 'container',
                 padding: {
                   top: 5,
@@ -33,34 +34,29 @@ export function send(app: App) {
                   left: 5
                 },
                 child: {
-                  type: 'button',
-                  child: {
-                    type: 'column',
-                    children: [
-                      {
-                        type: 'text',
-                        text: o
-                      },
-                      {
-                        type: 'progress',
-                        value: 0,
-                        total: 0
-                      }
-                    ]
-                  },
-                  on_click: {
-                    action: 'vote',
-                    value: { }
-                  }
+                  type: 'column',
+                  children: [
+                    {
+                      type: 'text',
+                      text: o
+                    },
+                    {
+                      type: 'progress',
+                      value: 0,
+                      total: 0
+                    }
+                  ]
                 }
-              } as blocks.Container))
-            ]
-          }
+              },
+              on_click: {
+                action: 'vote',
+                value: { }
+              }
+            } as blocks.Button))
+          ]
         }
-      });
-    } catch (err) {
-      app.log(err);
-    }
+      }
+    });
 
     ack();
   };
